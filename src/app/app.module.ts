e2c5afdb4a953b920ca3e24b33730601d7d3e3b5
athulprakash007 <athull362@gmail.com>
2022-11-21 13:15:18 -0800

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { PostallComponent } from './postall/postall.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myroutes:Routes=[
+
+{path:"",component: PostComponent},
+{path:"postall",component:PostallComponent}
+
+]
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostallComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
